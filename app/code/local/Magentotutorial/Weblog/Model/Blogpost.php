eeb6322923741c4adb249c68e043fdce0163d0e1
Thac <nhthac@gmail.com>
2014-12-05 15:15:13 +1300

    3rd commit - adding Model, controllers, sql

---
+++ app/code/local/Magentotutorial/Weblog/Model/Blogpost.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Magentotutorial_Weblog_Model_Blogpost extends Mage_Core_Model_Abstract {
+
+    protected function _construct() {
+        $this->_init('weblog/blogpost');
+    }
+
+}
---
+++ app/code/local/Magentotutorial/Weblog/Model/Resource/Setup.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Magentotutorial_Weblog_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup {
+    
+}
